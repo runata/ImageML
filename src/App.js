cb88ec3a8c470faff985857584465ec493319d03
Mykyta Plakhotin <nikita.plakhotin@gmail.com>
2018-05-17 19:38:36 -0400

    first day

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  particles: {
+      number: {
+        value: 30,
+        density: {
+            enable: true,
+            value_area: 800
+        }
+      }
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+              params={particleOptions}
+            />
         <Navigation/>
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     );
